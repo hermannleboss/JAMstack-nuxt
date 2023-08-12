73ede803b6f9f0d281ab8505ffe270271695e1ff
Hermann EHO <hermannleboss@gmail.com>
2023-08-12 13:19:58 +0000

    feat: setup storyblock in nuxt

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <pre>{{story}}</pre>
 </template>
+
+<script setup lang="ts">
+import {useStoryblok} from ".nuxt/imports";
+
+const story = await useStoryblok('home', {version: "draft"})
+</script>
+
+<!--<template>-->
+<!--    ds-->
+<!--    <StoryblokComponent v-if="story" :blok="story.content" />-->
+<!--</template>-->
