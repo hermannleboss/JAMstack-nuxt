557ee56555c08bc10620dfe06f215e46ade24a22
Hermann EHO <hermannleboss@gmail.com>
2023-08-12 18:41:03 +0000

    feat: FeaturedProjects

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 
 <script setup lang="ts">
 
-const story = await useStoryblok('home', {version: "draft"})
+const story = await useStoryblok('home', {version: "draft", resolve_relations: 'featuredProjects.projects'}, {resolveRelations: 'featuredProjects.projects'})
 </script>
 
 
---
+++ storyblok/FeaturedProjects.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+    blok: Object,
+})
+</script>
+<template>
+    <div class="container" v-editable="blok">
+        <h2 style="margin-left: 10px">{{ blok.title }}</h2>
+        <div class="featured-projects">
+            <div class="project" v-for="project in blok.projects" :key="project._uid">
+                <NuxtLink :to="{name:'projects-slug', params: {slug: project.slug}}">
+                    <img :src="project.content.image?.filename || 'https://picsum.photos/300/175'"
+                         :alt="project.content.image?.alt">
+                    <h3>{{ project.content.name }}</h3>
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
