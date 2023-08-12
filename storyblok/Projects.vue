d999d1bad35f540b1e13ec3f76a2e4366eefd496
Hermann EHO <hermannleboss@gmail.com>
2023-08-12 18:10:46 +0000

    feat: projects component

---
+++ storyblok/Projects.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+    blok: Object,
+})
+</script>
+<template>
+        <div class="container project-page">
+            <div class="project-title">
+                <h1>{{blok.name}}</h1>
+                <Nuxtlink :to="{name:'index'}">Go Back</Nuxtlink>
+            </div>
+            <div class="project-description">
+                <img :src="blok.image.filename" :alt="blok.image.alt">
+                <p>{{blok.description}}</p>
+            </div>
+        </div>
+</template>
