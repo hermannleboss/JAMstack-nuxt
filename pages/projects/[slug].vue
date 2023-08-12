59708032cc12e10ec367404e0f724d0914c0a2be
Hermann EHO <hermannleboss@gmail.com>
2023-08-12 18:04:35 +0000

    feat: projects page

---
+++ pages/projects/[slug].vue
@@ -0,0 +1,9 @@
+<template>
+    <pre>{{story}}</pre>
+    <StoryblokComponent v-if="story" :blok="story.content"></StoryblokComponent>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const story = await useStoryblok(`projects/${route.params.slug}`, {version: "draft"})
+</script>
