3e3f90efcea7cf4eef07fcf0bb0627436280cd28
Hermann EHO <hermannleboss@gmail.com>
2023-08-12 18:04:04 +0000

    refactor: enable routing

--- app.vue
+++ app.vue
@@ -1,15 +1,3 @@
 <template>
-<!--    <pre>{{story}}</pre>-->
-    <StoryblokComponent  v-if="story" :blok="story.content"></StoryblokComponent >
+    <NuxtPage></NuxtPage>
 </template>
-
-<script setup lang="ts">
-import {useStoryblok} from ".nuxt/imports";
-
-const story = await useStoryblok('home', {version: "draft"})
-</script>
-
-<!--<template>-->
-<!--    ds-->
-<!--    <StoryblokComponent v-if="story" :blok="story.content" />-->
-<!--</template>-->
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <StoryblokComponent  v-if="story" :blok="story.content"></StoryblokComponent >
+</template>
+
+<script setup lang="ts">
+
+const story = await useStoryblok('home', {version: "draft"})
+</script>
+
+
+<style scoped>
+
+</style>
